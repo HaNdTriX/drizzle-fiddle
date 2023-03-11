e33eb248f68e8998c63c914eb4e6d8fc3839dd50
Henrik Wenz <henrik.wenz@gmail.com>
2023-03-11 10:15:56 +0100

    Reuse MySQL connection pool

--- db/index.ts
+++ db/index.ts
@@ -1,12 +1,23 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 
-const poolConnection = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  database: process.env.MYSQL_DATABASE,
-  password: process.env.MYSQL_PASSWORD,
-});
+// Attach the pool connection to the global object so that it can be reused
+// across lambda invocations. This solution is far from ideal, but it's the
+// only way to get around the fact that lambda functions are stateless.
+// We sill might hit connection limits, so we should think about some kind of
+// bouncer proxy that allows us to reuse connections.
+const globalForMysql = globalThis as unknown as {
+  mysqlPoolConnection: mysql.Pool;
+};
+
+export const poolConnection =
+  globalForMysql.mysqlPoolConnection ||
+  mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_PASSWORD,
+  });
 
 const db = drizzle(poolConnection);
 
